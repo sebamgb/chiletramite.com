9c6f60ed7ac823b8f2e3181402197623de62314c
debianAdmin <sebamgb@gmail.com>
2024-04-20 22:25:25 -0400

    first commit -- new blog site with site in astro.config.mjs changed, changed src/data/site.config.ts title and language, disqus enabled and shortname added in src/data/disqus.config.ts, delete and changed categories  in src/data/categories.ts, changed max characters for title in src/content/config.ts and posts with assets added

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -10,10 +10,10 @@ interface SiteConfig {
 
 export const siteConfig: SiteConfig = {
 	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
+	title: 'Chiletramites, el cómo de los trámites en línea de chile', // Site title.
 	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
+	lang: 'es_CL',
+	ogLocale: 'es_CL',
 	shareMessage: 'Share this post', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
 }
